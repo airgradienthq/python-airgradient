6f5f87d49d4dffac0075f42d9514f295b4283fef
Joostlek <joostlek@outlook.com>
2024-05-23 09:32:33 +0200

    Fix json mapping

--- src/airgradient/models.py
+++ src/airgradient/models.py
@@ -26,7 +26,7 @@ class Measures(DataClassORJSONMixin):
         default=None, metadata=field_options(alias="tvocIndex")
     )
     raw_total_volatile_organic_component: int | None = field(
-        default=None, metadata=field_options(alias="tvoc_raw")
+        default=None, metadata=field_options(alias="tvocRaw")
     )
     pm003_count: int | None = field(
         default=None, metadata=field_options(alias="pm003Count")
@@ -35,7 +35,7 @@ class Measures(DataClassORJSONMixin):
         default=None, metadata=field_options(alias="noxIndex")
     )
     raw_nitrogen: int | None = field(
-        default=None, metadata=field_options(alias="nox_raw")
+        default=None, metadata=field_options(alias="noxRaw")
     )
     ambient_temperature: float | None = field(
         default=None, metadata=field_options(alias="atmp")
