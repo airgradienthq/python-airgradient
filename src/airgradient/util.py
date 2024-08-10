edbeab6c4e3a6950c96f82a352b2d9d6bfe74c19
Joost Lekkerkerker <joostlek@outlook.com>
2024-08-10 12:53:43 +0200

    Fix O-1PST not having model name (#173)

--- src/airgradient/util.py
+++ src/airgradient/util.py
@@ -7,7 +7,7 @@ def get_model_name(model_id: str) -> str | None:
     """Get model name from identifier."""
     if model_id.startswith("I-9PSL"):
         return "AirGradient ONE"
-    if model_id.startswith("O-1PPT"):
+    if model_id.startswith("O-1"):
         return "AirGradient Open Air"
     if "DIY" in model_id:
         return "AirGradient DIY"
